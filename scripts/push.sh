70765ac22a3e2611d1ba47df08c6a269d51180a5
Hugh McDonald <hmcdonald@hubspot.com>
2021-05-25 13:26:37 -0400

    Fix typo

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,4 +6,4 @@ if [[ ! "$1" ]]; then
 fi
 
 docker buildx create --use --config=buildkitd.toml
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $1/webalert --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $1/inventory-alert --push .
