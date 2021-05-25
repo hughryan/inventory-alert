99a8b7fb2c41d74d9da13a1da0097282760e2201
Hugh McDonald <hmcdonald@hubspot.com>
2021-05-24 22:18:00 -0400

    Add config

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,5 +5,5 @@ if [[ ! "$1" ]]; then
   exit 1
 fi
 
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $1/inventory-alert --push .
+docker buildx create --use --config=buildkitd.toml
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $1/webalert --push .
