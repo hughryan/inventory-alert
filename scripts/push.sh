589f06dbabb26fccba489f02be3a9f1f72bc3355
Hugh McDonald <hmcdonald@hubspot.com>
2021-05-24 22:09:44 -0400

    Bugfix

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,6 +5,4 @@ if [[ ! "$1" ]]; then
   exit 1
 fi
 
-docker buildx rm inventory-alert-builder
-docker buildx create --config=buildkitd.toml --use --name inventory-alert-builder
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $1/inventory-alert --push .
